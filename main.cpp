a4e5d24edc7e44a2eadd75b13f93a7b79c7b3141
Paranoic <62416155+SumnitelniyDenisSergeevich@users.noreply.github.com>
2021-04-17 00:02:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,57 +1,20 @@
-﻿#include "search_server_tests.h"
+#include "search_server_tests.h"
 #include "paginator.h"
 #include "read_input_functions.h"
 #include "request_queue.h"
-#include "log_duration.h"
-
-using namespace std::literals;
 
 int main() {
     TestSearchServer();
-    setlocale(LC_ALL, "Russian");
-    SearchServer search_server("и в на"s);
 
-    AddDocument(search_server, 1, "пушистый кот пушистый хвост"s, DocumentStatus::ACTUAL, { 7, 2, 7 });
-    AddDocument(search_server, 1, "пушистый пёс и модный ошейник"s, DocumentStatus::ACTUAL, { 1, 2 });
-    AddDocument(search_server, -1, "пушистый пёс и модный ошейник"s, DocumentStatus::ACTUAL, { 1, 2 });
-    AddDocument(search_server, 3, "большой пёс скво\x12рец евгений"s, DocumentStatus::ACTUAL, { 1, 3, 2 });
-    AddDocument(search_server, 4, "большой пёс скворец евгений"s, DocumentStatus::ACTUAL, { 1, 1, 1 });
-    std::cout << std::endl;
-    {
-        LOG_DURATION_STREAM("Operation time"s, std::cout);
-        FindTopDocuments(search_server, "пушистый -пёс"s);
-    }
-    std::cout << std::endl;
-    {
-        LOG_DURATION_STREAM("Operation time"s, std::cout);
-        FindTopDocuments(search_server, "пушистый --кот"s);
-    }
-    std::cout << std::endl;
-    {
-        LOG_DURATION_STREAM("Operation time"s, std::cout);
-        FindTopDocuments(search_server, "пушистый -"s);
-    }
-    std::cout << std::endl;
+    search_server.AddDocument(4, std::string{ "big dog sparrow Eugene" }, DocumentStatus::ACTUAL, { 1, 3, 2 });
+    search_server.AddDocument(5, std::string{ "big dog sparrow Vasiliy" }, DocumentStatus::ACTUAL, { 1, 1, 1 });
 
-    {
-        LOG_DURATION_STREAM("Operation time"s, std::cout);
-        MatchDocuments(search_server, "пушистый пёс"s);
+    for (int i = 0; i < 1439; ++i) {
+        request_queue.AddFindRequest(std::string{ "empty request" });
     }
-    std::cout << std::endl;
-    {
-        LOG_DURATION_STREAM("Operation time"s, std::cout);
-        MatchDocuments(search_server, "модный -кот"s);
-    }
-    std::cout << std::endl;
-    {
-        LOG_DURATION_STREAM("Operation time"s, std::cout);
-        MatchDocuments(search_server, "модный --пёс"s);
-    }
-    std::cout << std::endl;
-    {
-        LOG_DURATION_STREAM("Operation time"s, std::cout);
-        MatchDocuments(search_server, "пушистый - хвост"s);
-    }
-
+    request_queue.AddFindRequest(std::string{ "curly dog" });
+    request_queue.AddFindRequest(std::string{ "big collar" });
+    request_queue.AddFindRequest(std::string{ "sparrow" });
+    std::cout << std::string{ "Total empty requests: " } << request_queue.GetNoResultRequests() << std::endl;
     system("pause");
 }
